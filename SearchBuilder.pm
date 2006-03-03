31ef541c9f7415f5d190c83f93baca8912fae425
Alex Vandiver <alexmv@bestpractical.com>
2006-03-03 00:59:54 +0000

    Support for NOT STARTSWITH and NOT ENDSWITH

--- SearchBuilder.pm
+++ SearchBuilder.pm
@@ -740,11 +740,11 @@ sub Limit {
         }
         elsif ( $args{'OPERATOR'} =~ /STARTSWITH/i ) {
             $args{'VALUE'}    = $args{'VALUE'} . "%";
-            $args{'OPERATOR'} = "LIKE";
+            $args{'OPERATOR'} =~ s/STARTSWITH/LIKE/;
         }
         elsif ( $args{'OPERATOR'} =~ /ENDSWITH/i ) {
             $args{'VALUE'}    = "%" . $args{'VALUE'};
-            $args{'OPERATOR'} = "LIKE";
+            $args{'OPERATOR'} = s/ENDSWITH/LIKE/;
         } 
 	
 	$args{'OPERATOR'} =~ s/MATCHES/LIKE/i;  # MATCHES becomes LIKE, with no % stuff
