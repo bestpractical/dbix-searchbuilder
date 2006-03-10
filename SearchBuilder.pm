aa9811460d4785819ad1917e3da94397d5d96997
Ruslan Zakirov <ruz@bestpractical.com>
2006-03-10 14:14:53 +0000

    fix typo Changes

--- SearchBuilder.pm
+++ SearchBuilder.pm
@@ -4,7 +4,7 @@ package DBIx::SearchBuilder;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = "1.39";
+$VERSION = "1.40";
 
 use Clone qw();
 
@@ -744,7 +744,7 @@ sub Limit {
         }
         elsif ( $args{'OPERATOR'} =~ /ENDSWITH/i ) {
             $args{'VALUE'}    = "%" . $args{'VALUE'};
-            $args{'OPERATOR'} = s/ENDSWITH/LIKE/;
+            $args{'OPERATOR'} =~ s/ENDSWITH/LIKE/;
         } 
 	
 	$args{'OPERATOR'} =~ s/MATCHES/LIKE/i;  # MATCHES becomes LIKE, with no % stuff
