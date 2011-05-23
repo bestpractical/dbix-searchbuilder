bestpractical/dbix-searchbuilder

commit 32a174692fadf03e1aeb26581557717c5a21f52d

make it possible to provide function w/o field

lib/DBIx/SearchBuilder.pm
@@ -1521,8 +1521,11 @@ sub Column {
 
     $args{'ALIAS'} ||= 'main';
 
-    my $name = $args{ALIAS} . '.' . $args{FIELD};
-    if ( my $func = $args{FUNCTION} ) {
+    my $name;
+    if ( $args{FIELD} && $args{FUNCTION} ) {
+        $name = $args{'ALIAS'} .'.'. $args{'FIELD'};
+
+        my $func = $args{FUNCTION};
         if ( $func =~ /^DISTINCT\s*COUNT$/i ) {
             $name = "COUNT(DISTINCT $name)";
         }
@@ -1536,7 +1539,12 @@ sub Column {
         } else {
             $name = $func;
         }
-        
+    }
+    elsif ( $args{FUNCTION} ) {
+        $name = $args{FUNCTION};
+    }
+    else {
+        $name = 'NULL';
     }
 
     my $column = "col" . @{ $self->{columns} ||= [] };

commit 8f7a6c320a08ad220b03c00480e5d866f66c2d49

Fix the common use case of ->Column( FIELD => "foo" ) broken in 32a1746

lib/DBIx/SearchBuilder.pm
@@ -1596,6 +1596,9 @@ sub Column {
     elsif ( $args{FUNCTION} ) {
         $name = $args{FUNCTION};
     }
+    elsif ( $args{FIELD} ) {
+        $name = $args{'ALIAS'} .'.'. $args{'FIELD'};
+    }
     else {
         $name = 'NULL';
     }