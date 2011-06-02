a3fc5bf80e90331b2ea4978a76799cda094ff094
Ruslan Zakirov <ruz@bestpractical.com>
2011-06-03 03:16:29 +0400

    don't turn NULL into NULL(ALIAS.FIELD)

--- lib/DBIx/SearchBuilder.pm
+++ lib/DBIx/SearchBuilder.pm
@@ -1606,7 +1606,7 @@ sub CombineFunctionWithField {
     }
 
     # If we want to call a simple function on the column
-    elsif ( $func !~ /\(/ )  {
+    elsif ( $func !~ /\(/ && lc($func) ne 'null' )  {
         $func = "\U$func\E($field)";
     }
 
