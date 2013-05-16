bestpractical/dbix-searchbuilder

commit d97c2910fdb3dfc2452c8bb2c47d8160f272b80c

Reset the iterator position whenever a search is run
The current iterator position, if set, will most likely have no bearing
on the new search results and can cause falsely skipped items.  This
type of bug can happen when another Limit is added after some items have
been iterated already.

lib/DBIx/SearchBuilder.pm
@@ -232,8 +232,9 @@ sub _DoSearch {
 
     my $QueryString = $self->BuildSelectQuery();
 
-    # If we're about to redo the search, we need an empty set of items
+    # If we're about to redo the search, we need an empty set of items and a reset iterator
     delete $self->{'items'};
+    $self->{'itemscount'} = 0;
 
     my $records = $self->_Handle->SimpleQuery($QueryString);
     return 0 unless $records;

commit becafc713fb28e061f3d53425e493bcd166e6364

Failing tests demonstrating a bug in ->Last introduced by d97c291

t/01searches.t
@@ -7,7 +7,7 @@ use Test::More;
 BEGIN { require "t/utils.pl" }
 our (@AvailableDrivers);
 
-use constant TESTS_PER_DRIVER => 149;
+use constant TESTS_PER_DRIVER => 150;
 
 my $total = scalar(@AvailableDrivers) * TESTS_PER_DRIVER;
 plan tests => $total;
@@ -439,6 +439,12 @@ SKIP: {
         is( $u->_Value($id_alias), $u->id, "fetched id with custom alias" );
         ok $u->{fetched}{"\L$_"}, "fetched normal field $_" for keys %{$u->_ClassAccessible};
     }
+
+    # Last without running the search first
+    $users_obj = TestApp::Users->new( $handle );
+    $users_obj->UnLimit;
+    $users_obj->OrderBy( FIELD => "Login", ORDER => "ASC" );
+    is $users_obj->Last->Login, "obra", "Found last record correctly before search was run";
 
 	cleanup_schema( 'TestApp', $handle );
 }} # SKIP, foreach blocks