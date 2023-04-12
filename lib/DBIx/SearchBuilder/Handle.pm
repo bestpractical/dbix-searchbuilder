e3dc1c3445f4b587ba5f9e5c2830214439eb8185
Ronaldo Richieri <ronaldo@bestpractical.com>
2023-04-12 18:24:24 -0300

    Fix database version comparison
    The version comparison was broken for MySQL and MariaDB because it was
    comparing versions as decimal numbers instead of dot bersions, which
    broke comparisons with MariaDB 10.10+.

--- lib/DBIx/SearchBuilder/Handle.pm
+++ lib/DBIx/SearchBuilder/Handle.pm
@@ -7,6 +7,7 @@ use Carp qw(croak cluck);
 use DBI;
 use Class::ReturnValue;
 use Encode qw();
+use version;
 
 use DBIx::SearchBuilder::Util qw/ sorted_values /;
 
@@ -332,10 +333,10 @@ sub Disconnect  {
     # interacting with a disconnected handle, here we unset
     # dbh to inform other code that there is no connection any more.
     # See also https://github.com/perl5-dbi/DBD-mysql/issues/306
-
+    my ($version) = $self->{'database_version'} =~ /^(\d+\.\d+)/;
     if (   $self->isa('DBIx::SearchBuilder::Handle::mysql')
         && $self->{'database_version'} =~ /mariadb/i
-        && $self->{'database_version'} ge '10.2' )
+        && version->parse('v'.$version) > version->parse('v10.2') )
     {
         $self->dbh(undef);
     }
