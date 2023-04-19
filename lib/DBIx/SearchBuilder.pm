a7e31a326875076dcc0da2ad19ebd208a89d58c2
sunnavy <sunnavy@bestpractical.com>
2023-04-20 04:16:24 +0800

    Prep version 1.75

--- lib/DBIx/SearchBuilder.pm
+++ lib/DBIx/SearchBuilder.pm
@@ -4,7 +4,7 @@ package DBIx::SearchBuilder;
 use strict;
 use warnings;
 
-our $VERSION = "1.74";
+our $VERSION = "1.75";
 
 use Clone qw();
 use Encode qw();
@@ -2100,7 +2100,7 @@ or via the web at
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2001-2022, Best Practical Solutions LLC.
+Copyright (C) 2001-2023, Best Practical Solutions LLC.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
